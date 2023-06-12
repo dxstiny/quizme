b817d6588c3e44b230ea20c6f6dc276d63e8b64c
dxstiny <david.diashorta@gmail.com>
2023-06-12 14:10:47 +0200

    new: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
-import { fileURLToPath, URL } from "node:url";
+/*
+ * Copyright (c) 2023, reAudioPlayer ONE.
+ * Licenced under the GNU General Public License v3.0
+ */
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "QuizMe",
+                short_name: "QuizMe",
+                icons: [
+                    {
+                        src: "/quizme/favicon.png",
+                        sizes: "512x512"
+                    }
+                ],
+                start_url: "/quizme/",
+                display: "standalone",
+                description: "QuizMe is a free, open-source learning platform"
+            }
+        })
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
