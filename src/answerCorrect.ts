52bb86eee3305693c768e23accb93ea1a27db8c2
dxstiny <david.diashorta@gmail.com>
2023-06-12 13:38:23 +0200

    fix, textinput: apply ignore flags to any/all

--- src/answerCorrect.ts
+++ src/answerCorrect.ts
@@ -4,22 +4,29 @@ import type { INumberAnswerQuestion, ITextAnswerQuestion } from "./quiz";
 export const checkTextAnswer = (question: ITextAnswerQuestion) => {
     let answer = question.answer;
     let exactSolution = question.solution;
+    let solutionAll = question.solutionAll;
+    let solutionAny = question.solutionAny;
     if (answer == null) return false;
 
     if (question.ignoreCase) {
         answer = answer.toLowerCase();
         exactSolution = exactSolution.toLowerCase();
+        solutionAll = solutionAll?.map((s) => s.toLowerCase());
+        solutionAny = solutionAny?.map((s) => s.toLowerCase());
     }
     if (question.ignoreWhitespace) {
         answer = answer.replace(/\s/g, "");
         exactSolution = exactSolution.replace(/\s/g, "");
+        solutionAll = solutionAll?.map((s) => s.replace(/\s/g, ""));
+        solutionAny = solutionAny?.map((s) => s.replace(/\s/g, ""));
     }
     if (question.ignorePunctuation) {
-        answer = answer.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-        exactSolution = exactSolution.replace(
-            /[.,\/#!$%\^&\*;:{}=\-_`~()]/g,
-            ""
-        );
+        const regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+
+        answer = answer.replace(regex, "");
+        exactSolution = exactSolution.replace(regex, "");
+        solutionAll = solutionAll?.map((s) => s.replace(regex, ""));
+        solutionAny = solutionAny?.map((s) => s.replace(regex, ""));
     }
 
     if (question.solutionRegex) {
@@ -27,14 +34,12 @@ export const checkTextAnswer = (question: ITextAnswerQuestion) => {
         return regex.test(answer);
     }
 
-    if (question.solutionAll?.length) {
-        const solutions = question.solutionAll;
-        return solutions.every((solution) => answer?.includes(solution));
+    if (solutionAll?.length) {
+        return solutionAll.every((solution) => answer?.includes(solution));
     }
 
-    if (question.solutionAny?.length) {
-        const solutions = question.solutionAny;
-        return solutions.some((solution) => answer?.includes(solution));
+    if (solutionAny?.length) {
+        return solutionAny.some((solution) => answer?.includes(solution));
     }
 
     if (question.levenshtein) {
