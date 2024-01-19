80f47d2c498f260c12a5e03c484040c356bf1493
dxstiny <david.diashorta@gmail.com>
2024-01-19 17:54:35 +0100

    build update

--- dist/assets/Redirect-a0f9d98c.js
+++
@@ -1,1 +0,0 @@
-import{d as t,n as r,a as n}from"./index-c328ba18.js";const c=t({__name:"Redirect",setup(u){const e=r(),s=n();s.courses.length===0&&e.push("/");const o=s.courses[Math.floor(Math.random()*s.courses.length)].id;return e.push(`/quiz/${o}`),()=>{}}});export{c as default};
