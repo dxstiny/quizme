6d0c3dd5138930bdf10340c873dbbf55de1000ab
dxstiny <david.diashorta@gmail.com>
2024-07-03 17:33:22 +0200

    build update

---
+++ dist/assets/Redirect-C5r9BYnG.js
@@ -0,0 +1,1 @@
+import{d as t,e as r,s as u}from"./index-DcuYuW3j.js";const c=t({__name:"Redirect",setup(n){const e=u(),s=r();s.courses.length===0&&e.push("/");const o=s.courses[Math.floor(Math.random()*s.courses.length)].id;return e.push(`/quiz/${o}`),()=>{}}});export{c as default};
